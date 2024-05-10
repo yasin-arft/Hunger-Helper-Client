1391b775ee685315661a624939190c1d78596d6f
yasin-arft <yasinarftsocial@gmail.com>
2024-05-10 16:43:45 +0600

    navbar responsived

--- src/pages/shared/navbar/Navbar.jsx
+++ src/pages/shared/navbar/Navbar.jsx
@@ -3,7 +3,7 @@ import logo from '../../../assets/logo/hunger_helper_logo.png';
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
-  const user = true;
+  const user = false;
 
   const navItemData = [
     { path: '/', text: 'Home' },
@@ -25,15 +25,7 @@ const Navbar = () => {
 
   return (
     <div className="navbar bg-base-100 shadow">
-      <div className="navbar-start">
-        <div className="dropdown">
-          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
-            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
-          </div>
-          <ul tabIndex={0} className="gap-4 menu-sm dropdown-content mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
-            {navItems}
-          </ul>
-        </div>
+      <div className="navbar-start w-auto flex-1">
         <Link to={'/'}>
           <div className="flex gap-2 h-full items-center">
             <img src={logo} alt="logo" className="size-12" />
@@ -41,14 +33,34 @@ const Navbar = () => {
           </div>
         </Link>
       </div>
-      <div className="navbar-center hidden lg:flex">
-        <ul className="gap-3 menu-horizontal px-1">
+      <div className="navbar-center hidden lg:flex mr-3">
+        <ul className="menu-horizontal px-1">
           {navItems}
         </ul>
       </div>
-      <div className="navbar-end gap-2 items-center">
-        <button className="btn bg-blue-light hover:bg-blue-dark text-white font-semibold text-base">Sign In</button>
-        <button className="btn bg-blue-light hover:bg-blue-dark text-white font-semibold text-base">Sign Up</button>
+      <div className="navbar-end items-center w-auto">
+        <div className="hidden lg:block">
+          <button className="btn bg-blue-light hover:bg-blue-dark text-white font-semibold text-base mr-3">Sign In</button>
+          <button className="btn bg-blue-light hover:bg-blue-dark text-white font-semibold text-base">Sign Up</button>
+        </div>
+        <div className="dropdown">
+          <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+            <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M4 6h16M4 12h8m-8 6h16" /></svg>
+          </div>
+          <div className="dropdown-content right-0 ">
+            <ul tabIndex={0} className="gap-4 menu-sm mt-3 z-[1] p-2 shadow bg-base-100 rounded-box w-52">
+              {navItems}
+            </ul>
+            <div className="flex flex-col">
+              {
+                user ? <button className="btn btn-sm bg-blue-light hover:bg-blue-dark text-white font-semibold text-base">Sing Out</button> : <>
+                  <button className="btn btn-sm bg-blue-light hover:bg-blue-dark text-white font-semibold text-base">Sign In</button>
+                  <button className="btn btn-sm bg-blue-light hover:bg-blue-dark text-white font-semibold text-base">Sign Up</button>
+                </>
+              }
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
