a032d737eec11a53605769a2e14fdfa5bef93dbb
yasin-arft <yasinarftsocial@gmail.com>
2024-05-15 13:54:29 +0600

    banner added

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,15 +1,17 @@
 import { Helmet } from "react-helmet-async";
 import FeaturedFoods from "./featuredFoods/FeaturedFoods";
+import Banner from "./banner/Banner";
 
 
 const Home = () => {
   return (
     <div>
-      
+
       <Helmet>
         <title>Hunger Helper | Home</title>
       </Helmet>
       
+      <Banner />
       <FeaturedFoods />
     </div>
   );
---
+++ src/pages/home/banner/Banner.jsx
@@ -0,0 +1,19 @@
+
+
+const Banner = () => {
+  return (
+    <section className="mb-5 mt-2 md:mb-8 lg:mb-10 rounded-md overflow-hidden">
+      <div className="hero min-h-52 md:h-[80vh]" style={{ backgroundImage: 'url(https://i.ibb.co/DYDJx3c/food-donation.jpg)' }}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Hunger Helper</h1>
+            <p className="mb-5">A non-profit food donation website. Working for bring ease in food donation related activities.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Banner;
